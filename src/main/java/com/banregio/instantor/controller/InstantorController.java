ac10eae7f7445a25f73345dec191fcc17397638d
gerson <victorgerson@gmail.com>
2017-01-13 12:48:49 -0600

    cambios para instantor

---
+++ src/main/java/com/banregio/instantor/controller/InstantorController.java
@@ -0,0 +1,24 @@
+package com.banregio.instantor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by VICTOR VALDEZ [gerson] on 13/01/17.
+ */
+
+@Controller
+public class InstantorController {
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String testInstantor(Model model, String error, String logout){
+        if (error != null)
+            model.addAttribute("error", "Your username and password is invalid.");
+
+        if (logout != null)
+            model.addAttribute("message", "You have been logged out successfully.");
+
+        return "login";
+    }
+}
